dd63bf1a977e32c7b1919c1de757ffb34b6573d6
Nestor Zepeda <NestorDZepeda@gmail.com>
2017-07-12 00:50:58 -0700

    Added modals to user sign up flow

--- client/js/emailcollector.js
+++ client/js/emailcollector.js
@@ -2,9 +2,27 @@ function emailFormButtonPressed(){
         var userEmail = $('#userEmail').val();
 
         if(userEmail !== ""){
-            $.get("http://"+window.location.hostname +"/saveEmail?email=" +userEmail, function(data, status){
-                // console.log(data);
-                // console.log(status);
-            });
+            // $.get("http://"+window.location.hostname +"/saveEmail?email=" +userEmail, function(data, status){
+            //     // console.log(data);
+            //     //
+            //      console.log(status);
+            // });
+
+            $.ajax({
+                type: "GET",
+                url: "http://"+ window.location.hostname + "/saveEmail?email=" + userEmail,
+                success: function(response){
+                    $.Zebra_Dialog('<strong>Thanks!</strong>, we\'ll send you updates and let you know when you can start using Lenditt!', {
+                        type: 'confirmation',
+                        title: 'Thank You!'
+                    });
+                },
+                error: function(error){
+                     $.Zebra_Dialog('Looks like you\'ve already signed up! We\'ll keep you updated with any news.', {
+                        type: 'error',
+                        title: 'Oops!'
+                    });
+                }
+            })
         }
 }
