6436aa3ababd90434a7e3a033e0b50f17200169f
Nestor Zepeda <NestorDZepeda@gmail.com>
2017-08-09 23:49:17 -0700

    Added slack notification when user signs up

--- server/controllers/postEmailToDbController.js
+++ server/controllers/postEmailToDbController.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose'),
 UserInterestEmail = mongoose.model('UserInterestEmail');
 
+var Slack = require('slack-node');
+
 exports.postUserEmailToDb = function(req, res){
     UserInterestEmail.create({email: req.query.email}, function(err, data){
         res.setHeader('Content-Type', 'application/json');
@@ -10,7 +12,24 @@ exports.postUserEmailToDb = function(req, res){
         }
         else{
             res.status(200).send(data);
+            postToSlack("New user email added: " + req.query.email);
         }
+    });
+}
+
+function postToSlack(text){
+    webhookUri = "https://hooks.slack.com/services/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";
+
+    slack = new Slack();
+    slack.setWebhook(webhookUri);
+
+    slack.webhook({
+        channel: "#website-email-signups",
+        username: "LendittBot",
+        icon_imoji: ":robot_face:",
+        text: text
+    }, function(err, response) {
+        console.log(response);
     });
 }
 
